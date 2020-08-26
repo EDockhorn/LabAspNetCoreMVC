d388a946a6c45dfe8f8199828b079ce8721042bb
EDockhorn <johnnyerick111@hotmail.com>
2020-08-25 21:21:52 -0300

    update

---
+++ CursoDominandoAspNetCore/CursoDominandoAspNetCore/Data/CursoDominandoAspNetCoreContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoDominandoAspNetCore.Models
+{
+    public class CursoDominandoAspNetCoreContext : DbContext
+    {
+        public CursoDominandoAspNetCoreContext (DbContextOptions<CursoDominandoAspNetCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CursoDominandoAspNetCore.Models.Movie> Movie { get; set; }
+    }
+}
--- CursoDominandoAspNetCore/CursoDominandoAspNetCore/Startup.cs
+++ CursoDominandoAspNetCore/CursoDominandoAspNetCore/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using CursoDominandoAspNetCore.Models;
 
 namespace CursoDominandoAspNetCore
 {
@@ -33,6 +35,9 @@ namespace CursoDominandoAspNetCore
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<CursoDominandoAspNetCoreContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("CursoDominandoAspNetCoreContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
